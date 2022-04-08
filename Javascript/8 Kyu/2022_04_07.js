8f8718e77a2abcba93406bf8cae0b27f72899870
Bilal Shah <shahb1996@gmail.com>
2022-04-08 00:02:39 -0400

    updated two challenges in 04_07 challenges

--- Javascript/8 Kyu/2022_04_07.js
+++ Javascript/8 Kyu/2022_04_07.js
@@ -12,3 +12,17 @@ function firstNonConsecutive (arr) {
 // 8 Kyu - Basic Training: Add Item to an Array
 
 websites.push("codewars") // add the value of "codewars" to the "webites" array
+
+// 8 Kyu - A Wolf in Sheep's Clothing
+
+function warnTheSheep(queue) {
+  const newQueue = queue.reverse().indexOf('wolf') // reverse original queue and produce index value of wolf
+  return newQueue === 0 ? 'Pls go away and stop eating my sheep' : `Oi! Sheep number ${ newQueue }! You are about to be eaten by a wolf!`; // if wolf's index is 0, return first statement. Otherwise, return second statement
+}
+
+// 8 Kyu - Grasshopper - Array Mean
+
+var findAverage = function (nums) {
+  let number = nums.reduce( (acc,c) => acc + c, 0 ) // add all the values in the given array
+  return number/nums.length // return the sum of values divided by the array length
+}
