13d10f141d3c735bbceb9bd26bc49aefa8734aef
Bilal Shah <shahb1996@gmail.com>
2022-05-22 20:24:47 -0400

    added 05_22 challenges

---
+++ Javascript/8 Kyu/2022_05_22.js
@@ -0,0 +1,7 @@
+// 8 Kyu - Is It a Palindrome?
+
+/* Write a function that checks if a given string (case insensitive) is a palindrome. */
+
+function isPalindrome(x) {
+    return x.toLowerCase() == x.toLowerCase().split("").reverse().join("");
+}
