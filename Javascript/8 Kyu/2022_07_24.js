d0a414dc858131b04db201044cef5a10357677ae
Bilal Shah <shahb1996@gmail.com>
2022-07-24 23:58:03 -0400

    added 07_24 challenge

---
+++ Javascript/8 Kyu/2022_07_24.js
@@ -0,0 +1,7 @@
+// 8 Kyu - Bin to Decimal
+
+/* Complete the function which converts a binary number (given as a string) to a decimal number. */
+
+function binToDec(bin){
+    return parseInt(bin,2);
+}
