f6d01f91320fc8469c70de7a1f209c7e8b63e07b
Bilal Shah <shahb1996@gmail.com>
2022-08-03 22:53:28 -0400

    added 08_03 challenge

---
+++ Javascript/7 Kyu/2022_08_03.js
@@ -0,0 +1,24 @@
+// 7 Kyu - By 3, or not by 3? That is the Question...
+
+
+/* A trick I learned in elementary school to determine whether or not a number was divisible by three is to add all of the integers in the number together and to divide the resulting sum by three. If there is no remainder from dividing the sum by three, then the original number is divisible by three as well.
+
+Given a series of digits as a string, determine if the number represented by the string is divisible by three.
+
+Example:
+
+"123"      -> true
+"8409"     -> true
+"100853"   -> false
+"33333333" -> true
+"7"        -> false */
+
+function divisibleByThree(str){
+    let array = String(str).split("")
+    let newArray = array.map(x => Number(x))
+    let sum = 0;
+    for(let i = 0; i < newArray.length; i++) {
+      sum += newArray[i]
+    }
+    return sum % 3 === 0
+}
