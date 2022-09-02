6b7d020b8da5a9ffed3568c40b79e225231b89a4
Bilal Shah <shahb1996@gmail.com>
2022-09-01 22:01:06 -0700

    added 09_01 challenge

---
+++ Javascript/8 Kyu/2022_09_01.js
@@ -0,0 +1,21 @@
+// 8 Kyu - Pythagorean Triple
+
+/* Given an array of 3 non-negative integers a, b and c, determine if they form a pythagorean triple.
+
+A pythagorean triple is formed when:
+
+c2 = a2 + b2
+where c is the largest value of a, b, c.
+
+For example: a = 3, b = 4, c = 5 forms a pythagorean triple, because 52 = 32 + 42
+
+Return Values
+1 if a, b and c form a pythagorean triple
+0 if a, b and c do not form a pythagorean triple
+For Python: return True or False
+For JavaScript: return true or false */
+
+function isPythagoreanTriple(integers) {
+    let sorted = integers.sort((a,b) => a - b)
+    return Math.pow(sorted[2], 2) === Math.pow(sorted[0], 2) + Math.pow(sorted[1], 2)
+}
