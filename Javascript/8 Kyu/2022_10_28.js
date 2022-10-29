4450faf376fdcd9c62c09f3c64d60a78693dd7b5
Bilal Shah <shahb1996@gmail.com>
2022-10-28 22:50:03 -0400

    added 10_28 challenge

---
+++ Javascript/8 Kyu/2022_10_28.js
@@ -0,0 +1,19 @@
+// 8 Kyu - Pillars
+
+/* There are pillars near the road. The distance between the pillars is the same and the width of the pillars is the same. Your function accepts three arguments:
+
+number of pillars (≥ 1);
+distance between pillars (10 - 30 meters);
+width of the pillar (10 - 50 centimeters).
+Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar). */
+
+function pillars(numPill, dist, width) {
+    if(numPill === 1) {
+      return 0
+    }
+    
+    let distConversion = dist * 100;
+    let pillarsBetweenDist = (numPill - 2) * distConversion
+    let pillarsBetweenWidth = (numPill - 2) * width
+    return pillarsBetweenDist + pillarsBetweenWidth + distConversion
+}
